7547995d0a6d92f37c78fdd7b422382edeb4e01a
Nader Ghassemi <nad.ghasemi@gmail.com>
2018-03-18 12:33:03 -0500

    part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,12 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def title_header_class(params)
+    if params[:sort] == 'title' then 'hilite' end
+  end
+
+  def release_date_header_class(params)
+    if params[:sort] == 'release_date' then 'hilite' end
+  end
 end
