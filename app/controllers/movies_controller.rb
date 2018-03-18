ed540ec89428c1f5a3e89701a21e86e839526fda
Nader Ghassemi <nad.ghasemi@gmail.com>
2018-03-18 12:53:34 -0500

    Adding some Add clarifications

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,30 +15,31 @@ class MoviesController < ApplicationController
     redirect = false
 
     if params.has_key?(:sort) && !params[:sort].nil?
-      @sort = params[:sort]
+      @sort = params[:sort] # Get sorting info from URL params
       session[:sort] = @sort
     elsif session.has_key?(:sort) && !session[:sort].nil?
-      @sort = session[:sort]
+      @sort = session[:sort] # Get sorting info from session cookie
       redirect = true
     else
-      @sort = :id
+      @sort = :id # Just sort by ID by default
     end
 
     if params.has_key?(:ratings) && !params[:ratings].nil?
-      @checked_ratings = params[:ratings].keys
+      @checked_ratings = params[:ratings].keys # Get checked ratings from URL params
       session[:checked_ratings] = @checked_ratings
     elsif session.has_key?(:checked_ratings) && !session[:checked_ratings].nil?
-      @checked_ratings = session[:checked_ratings]
+      @checked_ratings = session[:checked_ratings] # Get checked ratings from session cookie
       redirect = true
     else
-      @checked_ratings = @all_ratings
+      @checked_ratings = @all_ratings # All ratings checked by default
     end
 
-    if redirect
-      flash.keep
+    if redirect # To keep it RESTful, we may need to redirect using the session params
+      flash.keep # Don't overwrite a recent flash notice
       redirect_to movies_path(:sort => @sort, :ratings => Hash[@checked_ratings.map {|rating| [rating, 1]}])
     end
 
+    # Do a single database query with ordering and filtering
     @movies = Movie.where({rating: @checked_ratings}).order(@sort)
   end
 
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,10 +4,12 @@ module MoviesHelper
     count.odd? ?  "odd" :  "even"
   end
 
+  # Checks if the header needs to be hilited
   def header_class(header)
     if @sort == header then 'hilite' end
   end
 
+  # Checks if a rating is checked
   def checked(rating)
     if @checked_ratings.nil?
         @checked_ratings = Movie.all_ratings
